0e62604804b6353eb099a91dfccc9fd8231caff2
lkod2 <loowfi2@gmail.com>
2025-04-19 02:55:00 -0400

    arreglos css

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,11 @@
+// menu
 
+document.getElementById("menu-icon").addEventListener("click", function() {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("active");
+});
+
+// DARK MODE
 document.getElementById("tema").addEventListener("click", function() {
     document.body.classList.toggle("dark");
     if(document.body.classList.contains("dark")){
@@ -20,4 +27,14 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener('DOMContentLoaded', function() {
     const aboutSection = document.querySelector('.about-content');
     aboutSection.classList.add('animate');
+});
+
+
+document.getElementById("colores").addEventListener("click", function() {
+    document.body.classList.toggle("colorful");
+    if(document.body.classList.contains("colorful")){
+        localStorage.setItem("colorful-mode", "true");
+    } else {
+        localStorage.setItem("colorful-mode", "false");
+    }
 });
